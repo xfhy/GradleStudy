b3979bf4bee1dde892b2b0a6bd7f00b41717834c
xfhy <xfhy666@qq.com>
2020-07-19 23:13:50 +0800

    transform 未完

---
+++ buildSrc/src/main/groovy/transform/MyCustomTransform.groovy
@@ -0,0 +1,27 @@
+package transform
+
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+
+class MyCustomTransform extends Transform {
+
+    @Override
+    String getName() {
+        return null
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return null
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return null
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+}
